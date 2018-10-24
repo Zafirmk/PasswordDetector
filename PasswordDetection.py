07237a90b2345bf515a52d8db73f35f508a828d9
Zafir Khalid <42074951+Zafirmk@users.noreply.github.com>
2018-10-24 20:46:38 +0400

    Add files via upload

---
+++ PasswordDetection.py
@@ -0,0 +1,10 @@
+Pass = raw_input("Enter Password: ")
+def password_checker(Pass):
+    import re
+    PasswordRegex = re.compile(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$")
+    X = PasswordRegex.search(Pass)
+    if X == None:
+        print("Invalid, Type another password")
+    else:
+        print("Valid password")
+password_checker(Pass)
